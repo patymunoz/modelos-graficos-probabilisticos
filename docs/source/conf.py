928723e7fe8bfc493c220501312a0a4565a30ca0
patymunoz <munpaty@gmail.com>
2025-07-28 19:10:48 -0600

    prueba3000

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -87,3 +87,6 @@ thebe_config = {
 #    "https://unpkg.com/thebe@latest/lib/index.min.js",
 #    "init_thebe.js"
 #]
+
+def setup(app):
+    app.add_config_value("thebe_config", thebe_config, "html")
